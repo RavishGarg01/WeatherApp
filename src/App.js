c1e7732b1c1f6efeeed19ffb0d5fe23553d8a79f
RavishGarg01 <gargravish0106@gmil.com>
2023-05-11 12:42:01 +0530

    Deployed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import Tempapp from './components/tempapp';
+import Tempapp from './Components/Tempapp';
+
 
 function App() {
   return (
--- src/components/tempapp.js
+++ src/components/tempapp.js
@@ -1,16 +1,60 @@
-import React from 'react'
-import './css/style.css'
+import React, { useEffect, useState } from 'react';
+import '../Css/module.css';
 export default function Tempapp() {
+
+  const [city , setCity] = useState(null);
+  const [search , setSearch] = useState("Jalandhar");
+
+  useEffect(()=>{
+    
+    const fetchApi = async()=>{
+      const Url =`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=9f825fa32169253bee069806314d4d4a`;
+      const response = await fetch(Url);
+      const resJson = await response.json();
+      console.log(resJson)
+      setCity(resJson.main);
+
+    }
+
+
+    fetchApi();
+
+  },[search])
+
   return (
+
+  
+
     <div>
-       <div class="main">
-          <div class="box text-center">
-            <input type="text" placeholder="Search City" />
-            <button type="button" class="btn btn-success">Search</button>
-            <h2> Temprature At Jalandhar</h2>
-            <h1>19 C</h1>
-            <h4>Min-Temprature : 15 C | Max-Temprature : 25 C</h4>
-          </div>
+       <div className="main">
+          <div className="box text-center">
+            <input type="text" onChange={(event)=>{
+              setSearch(event.target.value); 
+            }} 
+              placeholder="Search City" />
+
+              {
+                !city?(
+                  <p>NO Data Found</p>
+                ) :(
+                  <>
+                  <div className="info">
+                  <h2> <i class="fa-solid fa-street-view fa-xl"></i>  {search}</h2>
+                   <h1 className='temp'>{city.temp}°C</h1>
+                   </div>
+                   <br/>
+                   <br/>
+       
+                   <p>Min-Temprature : {city.temp_min} °C </p>    
+                   <p>Max-Temprature : {city.temp_max} °C</p>
+                 
+                 </>
+                )
+              }
+            </div>
+             
+
+           
         </div>
     </div>
   )
